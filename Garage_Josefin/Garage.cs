6269d98f43dcb02b7da08570ce8753f853c7b34c
Josefin - elevdator <Josefin@DESKTOP-RRN9P3L>
2020-03-31 11:43:35 +0200

    Add project files.

---
+++ Garage_Josefin/Garage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage_Josefin
+{
+    class Garage
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
