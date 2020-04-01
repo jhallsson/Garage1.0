1345ca82200df6a64d5f983965445361109a863b
Josefin - elevdator <Josefin@DESKTOP-RRN9P3L>
2020-04-01 15:21:56 +0200

    UI meny börjad

--- Garage_Josefin/UI.cs
+++ Garage_Josefin/UI.cs
@@ -6,5 +6,37 @@ namespace Garage_Josefin
 {
     class UI
     {
+        public void Menu()
+        {
+            bool running = true;
+            GarageHandler handler = new GarageHandler();
+            do
+            {
+                Console.WriteLine("Welcome to the Garage App! Start by creating a garage.\nCapacity: ");
+                int.TryParse(Console.ReadLine(), out int capacity);
+                handler.CreateGarage(capacity);
+                //ToDo: check input, check success
+                Console.WriteLine("Garage succesfully built!" +
+                    "\nNavigate through the menu by selecting a number." +
+                    "\n1. Park a vehicle" +
+                    "\n2. Let a vehicle leave" +
+                    "\n3. List all vehicles in garage" +
+                    "\n4. List all vehicles of the same type" +
+                    "\n5. Search for a vehicle" +
+                    "\n0. Close down the Garage App");
+                        
+                char input = ' ';
+                switch (input)
+                {
+                    case '1': 
+                    case '0': 
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (running);
+        }
     }
 }
